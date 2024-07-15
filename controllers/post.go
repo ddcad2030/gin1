757b30717c5631ab2b4bfc3fc77c632639eaf966
david <ddnn2026@gmail.com>
2024-07-15 16:06:36 +0700

    fn restful

--- controllers/post.go
+++ controllers/post.go
@@ -9,7 +9,13 @@ import (
 )
 
 func PostCreate(c *gin.Context) {
-	post := models.Post{Title: "Create", Body: "David"}
+	var bodyPost struct {
+		Title string
+		Body  string
+	}
+	c.Bind(&bodyPost)
+
+	post := models.Post{Title: bodyPost.Title, Body: bodyPost.Body}
 	result := initializers.DB.Create(&post)
 	if result.Error != nil {
 		c.JSON(http.StatusNotFound, "Not create")
@@ -20,11 +26,80 @@ func PostCreate(c *gin.Context) {
 }
 
 func PostGet(c *gin.Context) {
-	var post models.Post
+	var post []models.Post
 	result := initializers.DB.Find(&post)
 
 	if result.Error != nil {
-		c.JSON(http.StatusNotFound, "Not found data")
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Not found data",
+		})
+		return
 	}
-	c.JSON(http.StatusOK, result)
+	c.JSON(http.StatusOK, post)
+}
+
+func PostGetById(c *gin.Context) {
+	id := c.Param("id")
+	var post models.Post
+	result := initializers.DB.First(&post, id)
+
+	if result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Not found data",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, post)
+}
+
+func PostUpdateById(c *gin.Context) {
+	id := c.Param("id")
+	var post models.Post
+
+	result := initializers.DB.First(&post, id)
+	if result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Not found data",
+		})
+		return
+	}
+
+	var bodyPost struct {
+		Title string
+		Body  string
+	}
+	c.Bind(&bodyPost)
+
+	resultUpdate := initializers.DB.Model(&post).Updates(models.Post{Title: bodyPost.Title, Body: bodyPost.Body})
+
+	if resultUpdate.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Can not Update",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Update Success",
+		"data":    post,
+	})
+}
+
+func PostDelete(c *gin.Context) {
+	id := c.Param("id")
+	var post models.Post
+
+	result := initializers.DB.First(&post, id)
+	if result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Not found data",
+		})
+		return
+	}
+
+	initializers.DB.Delete(&post, id)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Delete Successful",
+	})
 }
--- main.go
+++ main.go
@@ -16,6 +16,10 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/post", controllers.PostGet)
+	r.GET("/post/:id", controllers.PostGetById)
+	r.PUT("/post/:id", controllers.PostUpdateById)
+	r.DELETE("/post/:id", controllers.PostDelete)
+
 	r.POST("/post", controllers.PostCreate)
 
 	r.Run()
