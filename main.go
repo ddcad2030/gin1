f27034a12310ea948a3e9c992dd91c4aae53bf45
david <ddnn2026@gmail.com>
2024-07-15 14:30:18 +0700

    env

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"example/gin-crud-1/initializers"
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
+
+func init() {
+	initializers.LoadEnv()
+
+}
 
 func main() {
 	fmt.Println("go crud postgres")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping",
+		})
+	})
+
+	r.Run()
 }
